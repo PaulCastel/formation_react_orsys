7d4ccc21b2894b123599606575136317c0a6ea76
PaulCastel <paul.castel@cgi.com>
2022-05-03 14:49:30 +0200

    props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,10 +5,20 @@ import Button from './components/ui/Button/Button';
 function App() {
   return (
     <div className="App">
-      {/*<Button text='Du texte'></Button>*/}
-      <Button>Du text children</Button>
-      <Button><div>text node</div></Button>
-      <Button><div>text node 1</div><div>text node 2</div></Button>
+      {/*<Button text='Du texte' bgColor='aquamarine' color='white'></Button>*/}
+      <Button bgColor='grey'>Du text children</Button>
+      <Button>
+        <div>text node</div>
+        </Button>
+      <Button 
+      onButtonClicked={() => {
+        console.log('btn cliqué');
+      }} 
+      style={{color: 'yellow'}}
+      >
+        <div>text node 1</div>
+        <div>text node 2</div>
+      </Button>
       {/*<Button/>*/}
     </div>
   );
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import style from './Button.module.css'
+import style from './Button.module.css';
+import PropTypes from 'prop-types';
 
 const Button = (props)=>{
-    return <button className={style.Button}>{props.children}</button>
+    return <button 
+        className={style.Button}
+        style={{...props.style, backgroundColor:props.bgColor, color:props.color}}
+        type={props.type}
+        onClick={(evt) => {
+            props.onButtonClicked()
+        }}
+    >
+        {props.children}
+    </button>
 }
 
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    bgColor: PropTypes.string,
+    color: PropTypes.string,
+    style: PropTypes.object,
+    type: PropTypes.string,
+    onButtonClicked: PropTypes.func.isRequired
+}
+
+Button.defaultProps={
+    type:'button',
+    onButtonClicked:()=>{}
+}
 export default Button
